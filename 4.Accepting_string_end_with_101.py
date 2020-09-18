6bff5a07c84c657c37e06a07d60fd76f7cd146eb
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-09-18 14:01:53 +0530

    Update 4.Accepting_string_end_with_101.py

--- 4.Accepting_string_end_with_101.py
+++ 4.Accepting_string_end_with_101.py
@@ -3,6 +3,6 @@ st = input("Enter the Regular Expression: ")
 if len(st) < 3:
     print("Rejected")
 elif st[-1] == "1" and st[-2] == "0" and st[-3] == "1":
-        print("Accepted")
+    print("Accepted")
 else:
     print("Rejected")
