1287de99e0a8a7224e1a7cf1fe141ae397fba1c7
Kartavya-verma <53134926+Kartavya-verma@users.noreply.github.com>
2020-09-18 13:40:57 +0530

    Update 2. 3_consecutive_ones_or_zeroes.py

--- 2. 3_consecutive_ones_or_zeroes.py
+++ 2. 3_consecutive_ones_or_zeroes.py
@@ -1,6 +1,6 @@
 st = input("Enter the string: ")
 flag = 0
-for i in range(len(s)-2):
+for i in range(len(st)-2):
     if (st[i] == '0' and st[i+1] == '0' and st[i+2] == '0') or (st[i] == '1' and st[i+1] == '1' and st[i+2] == '1'):
         print("Accepted")
         flag = 1
